c987d12b4fe97e9c9c47219990d9c501deda15a6
Sanju S Nair <sanjusnair@Sanjus-MacBook-Air.local>
2025-03-22 19:31:42 +0530

    folder restructure

--- src/python_basics/some_lists.py
+++ src/python_basics/some_lists.py
@@ -1,7 +1,8 @@
-listone = ['sanju','Rhea']
+listone = ['sanju','Rhea','Sanju']
 listtwo = ['chakku','muthu']
 
 listthree = [listone, listtwo]
 
 sanju_nickname = [sanju[0] for sanju in listthree]
-print(sanju_nickname)
+print(listone.index('sanju'))
+print(listone.count('sanju'))

79f533c37991a18725fd484a3cf2240c9396f9c3
Sanju S Nair <sanjusnair@Sanjus-MacBook-Air.local>
2025-03-22 17:03:00 +0530

    restructure training codes

---
+++ src/python_basics/some_dictionaries.py
@@ -0,0 +1,9 @@
+test_dictionary = {'key1': 'sanju', 'key2' : 'sanju2'}
+
+print(test_dictionary['key1'])
+
+#two important methods are to use items(), keys() and values()
+
+print(test_dictionary.keys())
+print(test_dictionary.values())
+print(test_dictionary.items())

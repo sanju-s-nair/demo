f2c5978956a76bcef1c7bbdc69b40567b3dd06ff
Sanju S Nair <sanjusnair@Sanjus-MacBook-Air.local>
2025-03-25 18:04:37 +0530

    some exercise questions

---
+++ src/python_basics/some_classes.py
@@ -0,0 +1,7 @@
+class Sanju:
+    def __init__(self, skills):
+        self.skills = skills
+
+x = Sanju(skills = 'jumping')
+
+print(x.skills)
---
+++ src/python_basics/some_sets.py
@@ -0,0 +1,15 @@
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(1)
+print(x)
+
+sanju = [1,2,3,4,5,1,1,1,1,1]
+print(set(sanju))
+
+a = True
+b = False
+c = None 
+#3 types of booleans, None is just a placeholder
